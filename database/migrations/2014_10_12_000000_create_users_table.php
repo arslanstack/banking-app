7d349a80d6817ba2dedcab0c35c087f72cec1317
Muhammad Arslan <being.arslan@outlook.com>
2024-02-26 13:04:16 -0800

    Laravel Pulse + User Migration + JWT setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,17 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('ssn')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->tinyText('address');
+            $table->integer('otp');
+            $table->tinyInteger('status')->default(0)->comment('0=inactive, 1=active');
+            $table->string('device_token')->nullable();
+            $table->tinyInteger('is_blocked')->default(0)->comment('0=unblocked, 1=blocked');
             $table->timestamps();
         });
     }
